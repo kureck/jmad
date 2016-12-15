690de77307a412d588aa02d384d90cf7662f9206
Eric Kureck <kureck@gmail.com>
2016-12-15 09:08:09 -0200

    wip - adds models

---
+++ jmad/jmad/solos/tests/test_models.py
@@ -0,0 +1,31 @@
+from django.test import TestCase
+
+from jmad.solos.models import Solo
+
+
+class SoloModelTestCase(TestCase):
+
+    def setUp(self):
+        self.solo = Solo.objects.create(
+            track='Falling in love with love',
+            artist='Oscar Peterson',
+            instrument='piano'
+        )
+
+        self.drum_solo = Solo.objects.create(
+           instrument='drums',
+           artist='Rich',
+           track='Bugle Call Rag'
+        )
+
+       self.bass_solo = Solo.objects.create(
+           instrument='saxophone',
+           artist='Coltrane',
+           track='Mr. PC'
+        )
+
+    def test_solo_basic(self):
+        """
+        Test the basic functionality of Solo
+        """
+        self.assertEqual(self.solo.artist, 'Oscar Peterson')
--- jmad/jmad/solos/tests/test_views.py
+++ jmad/jmad/solos/tests/test_views.py
@@ -1,4 +1,6 @@
 from django.test import TestCase, RequestFactory
+from django.db.models.query import QuerySet
+
 from jmad.solos.views import index
 
 
@@ -16,3 +18,18 @@ class IndexViewTestCase(TestCase):
         with self.assertTemplateUsed('solos/index.html'):
             response = index(request)
             self.assertEqual(response.status_code, 200)
+
+    def test_index_view_returns_solos(self):
+        """
+        Test that the index view will attempt to return
+        Solos if query parameters exist
+        """
+        response = self.client.get(
+            '/',
+            {'instrument': 'drums'}
+        )
+        solos = response.context['solos']
+
+        self.assertIs(type(solos), QuerySet)
+        self.assertEqual(len(solos), 1)
+        self.assertEqual(solos[0].artist, 'Rich')
